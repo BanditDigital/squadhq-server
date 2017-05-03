52fdf1818c37e5346c53b8019ac7db931da712da
Danny Varley <danny.varley@bwcgroup.co.uk>
2017-05-03 12:32:33 +0100

    Removed favicon, added watch script to package.json

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-// uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
