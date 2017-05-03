6462d72d961cdba0aa14c85d8832dce823f59337
Danny Varley <danny.varley@bwcgroup.co.uk>
2017-05-03 12:26:24 +0100

    Restructred project folders and laid some groundwork

---
+++ src/user/user.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/**
+ * @api {get} /user/:id Request User information
+ * @apiName GetUser
+ * @apiGroup User
+ *
+ * @apiParam {Number} id Users unique ID.
+ *
+ * @apiSuccess {String} firstname Firstname of the User.
+ * @apiSuccess {String} lastname  Lastname of the User.
+ */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
